0265868291a11d43a5f8dc5e94f03b65d4a6beca
unknown <jot.johnting@gmail.com>
2020-07-05 17:35:06 +0800

    block hp fix, shoot range chamge

---
+++ core/src/mindustry/ui/fragments/MapWaveFragment.java
@@ -0,0 +1,13 @@
+package mindustry.ui.fragments;
+
+import arc.scene.Group;
+
+public class MapWaveFragment extends Fragment {
+
+  @Override
+  public void build(Group parent) {
+    // TODO Auto-generated method stub
+
+  }
+  
+}
